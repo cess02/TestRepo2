97eed732713424eb8c032cdf63a0afe5a8e0e7ff
PrincessJoy <princess.yuu02@gmail.com>
2017-05-22 10:29:50 +0800

    ssssd

---
+++ TestRepo2/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TestRepo2.Startup))]
+namespace TestRepo2
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
